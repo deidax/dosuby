24c065f877b3cd6497173081cf16dfaaa39ce2a5
Sigma247 <khalildida93@gmail.com>
2023-01-31 20:53:07 +0100

    Dork interface

---
+++ src/core/application/use_cases/dorks_enumeration_use_case.py
@@ -0,0 +1,32 @@
+from src.core.application.input_dtos.target_input_dto import TargetInputDTO
+from src.interfaces.dork import Dork
+
+class DorksEnumerationUseCase:
+    """This is the Dork Enumeration Use Case.\n
+    it will be used to enumerate subdomains using search engine dorks
+    """
+    
+    def __init__(self, dork: Dork) -> None:
+        """Inject a search engine dork to be used
+
+        Args:
+            dork (Dork): search engine dork (GoogleDork, BingDork...).Must implement Dork interface
+        """
+        
+        if not isinstance(dork, Dork):
+            raise ValueError(f'dork param should be a Dork instance, not a {type(dork)}')
+        
+        self._dork = dork
+    
+    def execute(self, target: TargetInputDTO):
+        """Execute method will run the enumeration process
+
+        Args:
+            target (TargetInputDTO): url or domain to enumerate
+        """
+        
+        if not isinstance(target, TargetInputDTO):
+            raise ValueError(f'target param should be a TargetInputDTO instance, not a {type(target)}.')
+        
+        return ['fake.sometext.test', 'dork.sometext.test']
+    
--- test/core/application/use_cases/test_enumerate_subdomains_using_dorkes.py
+++ test/core/application/use_cases/test_enumerate_subdomains_using_dorkes.py
@@ -1,16 +1,41 @@
 import pytest
 from src.core.application.input_dtos.target_input_dto import TargetInputDTO
+from src.core.application.use_cases.dorks_enumeration_use_case import DorksEnumerationUseCase
+from src.interfaces.dork import Dork
 
 fake_uri = 'sometext.test'
 
+
 fake_subdomains = ['fake.sometext.test', 'dork.sometext.test']
+
+def test_enumerate_using_invalid_dork_use_case():
+    
+    
+    fake_dork = 'fake dork'
+    
+    with pytest.raises(ValueError):
+        DorksEnumerationUseCase(dork=fake_dork)
+
 
 def test_enumerate_using_dorks_use_case():
     
     target_input = TargetInputDTO(uri=fake_uri)
     
-    target_subdomains_use_case = DorkEnumerationUseCase(dork=fake_dork)
+    fake_dork = Dork()
+    
+    target_subdomains_use_case = DorksEnumerationUseCase(dork=fake_dork)
     
     target_subdomains_result = target_subdomains_use_case.execute(target=target_input)
     
     assert target_subdomains_result == fake_subdomains
+
+def test_invalid_target_dto_input_in_dorks_use_case_execute():
+    
+    invalid_target_input = 'Invalid target DTO'
+    
+    fake_dork = Dork()
+    
+    target_subdomains_use_case = DorksEnumerationUseCase(dork=fake_dork)
+    
+    with pytest.raises(ValueError):
+        target_subdomains_use_case.execute(target=invalid_target_input)
