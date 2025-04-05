5457fb433d53e431d6fd25847681b6837ba811bc
Sigma247 <khalildida93@gmail.com>
2025-04-05 20:45:22 +0000

    Vuln checking for CMS and Webservers

--- setup.py
+++ setup.py
@@ -8,16 +8,8 @@ with open('requirements.txt', 'r', encoding='utf-8') as f:
 
 setup(
     name='dosuby',
-    use_scm_version={
-        # "version_scheme": "post-release",
-        "local_scheme": "node-and-date",
-        "write_to": "dosuby/version.py",
-        "write_to_template": '__version__ = "{version}"',
-        "relative_to": __file__,
-        "tag_regex": r"^v[0-9]+\.[0-9]+\.[0-9]+$",
-    },
+    use_scm_version=True,  # Simplified configuration - details in pyproject.toml
     setup_requires=["setuptools_scm"],
-    # version='1.5.0',
     packages=find_packages(),
     install_requires=requirements,
     entry_points={
